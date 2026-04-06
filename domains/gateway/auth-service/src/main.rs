e5ce2d90d8abbc55313472f98109bb49dbf953c6
subh <subhajitchaudhury05@gmail.com>
2026-04-06 23:23:46 +0200

    feat : localhost redirection

--- domains/gateway/auth-service/src/main.rs
+++ domains/gateway/auth-service/src/main.rs
@@ -4,12 +4,9 @@ mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting auth-service on 0.0.0.0:8083");
-    HttpServer::new(|| {
-        App::new()
-            .configure(routes::init)
-    })
-    .bind(("0.0.0.0", 8083))?
-    .run()
-    .await
+    println!("Starting auth-service on 127.0.0.1:8083");
+    HttpServer::new(|| App::new().configure(routes::init))
+        .bind(("127.0.0.1", 8083))?
+        .run()
+        .await
 }
