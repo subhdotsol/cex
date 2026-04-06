51f94c7a0f74a9bb9ae8e1850ba5b5ac1c0ef67d
subh <subhajitchaudhury05@gmail.com>
2026-04-07 00:53:58 +0200

    feat : get user by email function added

--- crates/db/src/users.rs
+++ crates/db/src/users.rs
@@ -1,8 +1,18 @@
 // all user realted functions here
 
-use chrono::Utc;
-use sqlx::PgPool;
+use chrono::{DateTime, Utc};
+use sqlx::{FromRow, PgPool};
 use uuid::Uuid;
+
+#[derive(Debug, FromRow)]
+pub struct User {
+    pub id: Uuid,
+    pub email: String,
+    pub password: String,
+    pub balance: i64,
+    pub kyc_status: String,
+    pub created_at: DateTime<Utc>,
+}
 
 pub async fn create_user(pool: &PgPool, email: &str, password: &str) -> Result<(), sqlx::Error> {
     sqlx::query!(
@@ -21,4 +31,18 @@ pub async fn create_user(pool: &PgPool, email: &str, password: &str) -> Result<(
     .await?;
 
     Ok(())
+}
+
+pub async fn get_user_by_email(pool: &PgPool, email: &str) -> Result<Option<User>, sqlx::Error> {
+    sqlx::query_as!(
+        User,
+        r#"
+        SELECT id, email, password, balance, kyc_status, created_at
+        FROM users
+        WHERE email = $1
+        "#,
+        email
+    )
+    .fetch_optional(pool)
+    .await
 }
